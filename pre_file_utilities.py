67f51610254308fdde35242b890fe654891ce2af
Dunc1995 <dpbailey1995@outlook.com>
2020-07-22 11:43:30 +0100

    Adds helper modules for more maintainable processing of string data

--- pre_file_utilities.py
+++ pre_file_utilities.py
@@ -1,12 +1,16 @@
 #!/usr/bin/python3
 import argparse
-import src.prefileparser as file_parser
+import src.file_parser as file_parser
+import logging
 
 def main(file_path: str):
-    data = file_parser.process_pre_data(file_path)
+    data = file_parser.process_pre_file_data(file_path)
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Reads a .pre file such that precipitation data can be transformed and uploaded to a SQLite database.')
     parser.add_argument('--input_file_path', '-i', type=str, required=True, help='Input file for processing.')
+    parser.add_argument('--log_verbosity', '-v', type=int, default=3, help='Logging verbosity (1 to 5) - level 1 is most verbose, whilst level 5 only logs critical logs.')
     args = parser.parse_args()
+
+    logging.basicConfig(filename='pre_file_utilities.log', filemode='w', level=args.log_verbosity*10)
     main(args.input_file_path)
